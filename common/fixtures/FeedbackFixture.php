12ecb5738c84da7202ddaa837558af467ac30c5f
Ivan Shalaev <shalaevig@mail.ru>
2021-01-04 21:39:31 +0400

    add fixture

---
+++ common/fixtures/FeedbackFixture.php
@@ -0,0 +1,9 @@
+<?php
+namespace common\fixtures;
+
+use yii\test\ActiveFixture;
+
+class FeedbackFixture extends ActiveFixture
+{
+    public $modelClass = 'common\models\Feedback';
+}
---
+++ common/fixtures/data/feedback.php
@@ -0,0 +1,11 @@
+<?php
+return [
+    'feedback1' => [
+        'customer' => 'divan',
+        'phone' => '+7(999)999-99-99',
+    ],
+    'feedback2' => [
+        'customer' => 'anonymous',
+        'phone' => '+0(000)000-00-00',
+    ],
+];
